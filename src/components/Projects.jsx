34f38a9f2b03de161c668cdd73ae0256f8a98d26
Ayush-Petwal <ayushpetwal003@gmail.com>
2024-07-16 18:59:24 +0530

    new updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PROJECTS } from '../constants';
 import {motion} from 'framer-motion';
+import { CiLink } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa6";
 
 const Projects = () => {
   return (
@@ -19,6 +21,10 @@ const Projects = () => {
             transition = {{duration : 0.8}}
             className='w-full lg:w-1/4'>
               <img width={150} height={150} src={project.image} alt={project.title} className='mb-6 rounded' />
+              <div className='flex gap-6 mb-3'>
+                <a href={project.github} target='_blank'>< FaGithub className='text-2xl text-white ' /></a>
+                <a href={project.livelink} target='_blank'>< CiLink className='text-3xl text-white ' /></a>
+              </div>
             </motion.div>
           <motion.div
           whileInView = {{opacity : 1 , x : 0}}
@@ -27,6 +33,7 @@ const Projects = () => {
           className='w-full max-w-xl lg:w-3/4'>
             <h6 className='mb-2 font-semibold'>{project.title}</h6>
             <p className='mb-4 text-neutral-400'>{project.description} </p>
+            
             { project.technologies.map((tech , index) => (
               <span key={index} 
               className='mr-2 bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,38 +1,46 @@
-import project1 from "../assets/projects/project.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/Portfolio.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from "../assets/projects/project.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
 
 export const ABOUT_TEXT = `Hello👋 I am Ayush Petwal, a student at Graphic Era Hill University, Batch of 2025 , I am a dedicated and versatile front-end developer and competitive programmer with a passion for creating efficient and user-friendly web applications. With 1/2 years of experience, I have worked with a variety of technologies, including React , Typescript and Tailwind CSS. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of front-end development, I enjoy my most of time in competitive programmer and improve my Data Structure and Algorithm and Problem Solving Skills .`;
 
 export const PROJECTS = [
   {
-    title: "Plinkooo",
+    title: "VisuAlgo",
     image: project1,
     description:
-      "Plinkooo is a game where players drop a disc down a pegged board, aiming for it to land in high-value slots at the bottom. As the disc falls, it bounces unpredictably off the pegs, making the outcome largely based on chance.",
-    technologies: ["React", "Tailwind CSS", "TypeScript", "Maths"],
+      "This project will create an interactive tool to visualize how sorting and pathfinding algorithms work. It will allow users to see these algorithms in action, step-by-step, fostering a deeper understanding of their logic and efficiency.",
+    technologies: ["React", "Tailwind CSS", "Data Structure Algorithm"],
+    github : "https://github.com/Ayush-Petwal/VisuAlgo",
+    livelink : "https://visualgorithem.vercel.app/",
   },
   {
-    title: "Code 100X",
+    title: "Expense Tracking System",
     image: project2,
     description:
-      "Code 100X is the ultimate coding platform for interview preparation, featuring a curated collection of FAANG DSA questions that boost your interview readiness.",
-    technologies: ["Typescript", "React"],
+      "This project will develop a web application for users to effectively manage their finances through expense tracking. It will leverage the MERN stack (MongoDB, Express, React, Node.js) to create a robust and user-friendly experience.",
+    technologies: ["React", "NodeJs" , "ExpressJs" , "MongoDB"],
+    github : "https://github.com/Ayush-Petwal/Expence-Tracking-System",
+    livelink : "https://expencetrackingsystem.vercel.app/",
   },
   {
-    title: "Portfolio Website",
+    title: "Let’s Meet",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["React", "Tailwind CSS" , "Framer Motion"],
+      "Let's Meet is a web-based video conferencing platform built with Next.js, inspired by popular tools like Zoom. It aims to provide a seamless and efficient way for users to connect and collaborate virtually.",
+    technologies: ["NextJs", "Tailwind CSS" , "Shadcn"],
+    github : "https://github.com/Ayush-Petwal/lets_meet/",
+    livelink : "https://letsmeetayushpetwal.vercel.app/",
   },
   {
-    title: "Advance Sudoku Solver",
+    title: " Wine Quality Prediction",
     image: project4,
     description:
       "Sudoku solver using the backtracking algorithm. Users can input a Sudoku puzzle, and the solver will provide the completed solution",
-    technologies: ["HTML", "Tailwind CSS", "React", "Backtracking" ],
+    technologies: ["Python", "Machine Learning"],
+    github : "https://github.com/Ayush-Petwal/Wine-Quality-Prediction",
+    livelink : "https://github.com/Ayush-Petwal/Wine-Quality-Prediction",
   },
 ];
 
